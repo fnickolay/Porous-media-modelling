9328e7fb0c5c5df4aaccaefa7cb02c2f69117437
fnickolay <fadeev.na21@physics.msu.ru>
2025-03-21 20:57:43 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='svmc_pmm',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'subprocess',
+        'matplotlib',
+        'pathlib',
+        'json',
+        'jdata',
+        'sys',
+        'seaborn',
+        'math',
+        'tqdm',
+        'time',
+        'mpl_toolkits',
+        'skimage',
+        'pathlib'
+    ],
+    author='fnickolay',
+    description='Python-based functions for porous media light propagation modelling',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/fnickolay/Porous-media-modelling',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
